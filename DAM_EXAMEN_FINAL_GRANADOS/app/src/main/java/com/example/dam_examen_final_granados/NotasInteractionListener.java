47f578d3f140efce8cbfaa06659a7eef84c02414
Adriam <lgk9hd.10@gmail.com>
2023-07-17 15:13:27 -0500

    Primer commit - Login - Granados

---
+++ DAM_EXAMEN_FINAL_GRANADOS/app/src/main/java/com/example/dam_examen_final_granados/NotasInteractionListener.java
@@ -0,0 +1,11 @@
+package com.example.dam_examen_final_granados;
+
+public interface NotasInteractionListener{
+
+        void editNotaClick(Nota nota);
+        void eliminaNotaClick (Nota nota);
+        void favoritaNotaClick (Nota nota);
+
+
+
+}
